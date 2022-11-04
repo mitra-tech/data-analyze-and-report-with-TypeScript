c4d0dd765ff0870afaca0f7f41c436b6ffd1ae69
Mitra <davoudabadi.mitra@gmail.com>
2022-11-03 19:52:05 -0600

    the generics was replaced with MatchData

--- build/index.js
+++ build/index.js
@@ -1,8 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const CsvFileReder_1 = require("./CsvFileReder");
+const MatchReader_1 = require("./MatchReader");
 const MatchResult_1 = require("./MatchResult");
-const reader = new CsvFileReder_1.CsvFileReader("football.csv");
+const reader = new MatchReader_1.MatchReader("football.csv");
 reader.read();
 let manUnitedWins = 0;
 for (let match of reader.data) {
--- src/MatchReader.ts
+++ src/MatchReader.ts
@@ -2,16 +2,17 @@ import { CsvFileReader } from "./CsvFileReder";
 import { MatchResult } from "./MatchResult";
 import { dateStringToDate } from "./utils";
 
-export class MatchReader extends CsvFileReader {
-    mapRow(row: string[]): MatchData {
-        return [
-            dateStringToDate(row[0]),
-            row[1],
-            row[2],
-            parseInt(row[3]),
-            parseInt(row[4]),
-            row[5] as MatchResult, // 'H' , 'A', 'D'
-            row[6],
-          ];
-    }
+type MatchData = [Date, string, string, number, number, MatchResult, string];
+export class MatchReader extends CsvFileReader<MatchData> {
+  mapRow(row: string[]): MatchData {
+    return [
+      dateStringToDate(row[0]),
+      row[1],
+      row[2],
+      parseInt(row[3]),
+      parseInt(row[4]),
+      row[5] as MatchResult, // 'H' , 'A', 'D'
+      row[6],
+    ];
+  }
 }
