781557ae216f806304d62863ae1193075fe17f71
Mitra <davoudabadi.mitra@gmail.com>
2022-11-07 22:45:13 -0700

    MatchReader file refactored

--- src/MatchReader.ts
+++ src/MatchReader.ts
@@ -8,16 +8,21 @@ interface DataReader {
 }
 
 export class MatchReader {
+  matches: MatchData[] = [];
   constructor(public reader: DataReader) {}
-  //   .map((row: string[]): MatchData => {
-  //     return [
-  //         dateStringToDate(row[0]),
-  //         row[1],
-  //         row[2],
-  //         parseInt(row[3]),
-  //         parseInt(row[4]),
-  //         row[5] as MatchResult,
-  //         row[6]
-  //     ]
-  //   })
+  load(): void {
+    this.reader.read();
+    // the following line of code gives us a new array that should be saved somewhere(matches)
+    this.matches = this.reader.data.map((row: string[]): MatchData => {
+      return [
+        dateStringToDate(row[0]),
+        row[1],
+        row[2],
+        parseInt(row[3]),
+        parseInt(row[4]),
+        row[5] as MatchResult,
+        row[6],
+      ];
+    });
+  }
 }
